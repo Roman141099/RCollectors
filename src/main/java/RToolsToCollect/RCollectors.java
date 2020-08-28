0f3af1e062150f32339ec2961c1d2108ad2e3a67
Romzes <roma.grigorev.2015@inbox.ru>
2020-08-28 16:23:40 +0400

    Added new Collector

--- src/main/java/RToolsToCollect/RCollectors.java
+++ src/main/java/RToolsToCollect/RCollectors.java
@@ -2,6 +2,7 @@ package RToolsToCollect;
 
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
@@ -158,23 +159,23 @@ public class RCollectors {
 //    }
 
     /**
-     * @param keyMapper the function that accepts element of stream and returns an element of other type(it is a key in main map)
-     * @param mainCreate the supplier that returns target exit container
+     * @param keyMapper     the function that accepts element of stream and returns an element of other type(it is a key in main map)
+     * @param mainCreate    the supplier that returns target exit container
      * @param subMainCreate the supplier that returns target container of values in main map
-     * @param valueMapper the function that accepts element of stream and returns an element of other type(it is a value in sub container)
-     * @param finish the finisher function
-     * @param <T> type of input elements
-     * @param <TT> type of key in main map
-     * @param <M> type of sub container
-     * @param <R> type of finish result
-     * @param <E> type of elements in sub container
+     * @param valueMapper   the function that accepts element of stream and returns an element of other type(it is a value in sub container)
+     * @param finish        the finisher function
+     * @param <T>           type of input elements
+     * @param <TT>          type of key in main map
+     * @param <M>           type of sub container
+     * @param <R>           type of finish result
+     * @param <E>           type of elements in sub container
      * @return
      */
     public static <T, TT, M extends Collection<E>, E, R> Collector<T, ?, R> advancedGrouping(Function<? super T, ? extends TT> keyMapper,
                                                                                              Supplier<Map<TT, M>> mainCreate,
                                                                                              Supplier<M> subMainCreate,
-                                                                                             Function<? super T,? extends E> valueMapper,
-                                                                                             Function<Map<TT, M>,? extends R> finish) {
+                                                                                             Function<? super T, ? extends E> valueMapper,
+                                                                                             Function<Map<TT, M>, ? extends R> finish) {
         class Container {
             final Map<TT, M> exitMap;
 
@@ -217,6 +218,66 @@ public class RCollectors {
                 Container::add,
                 Container::combine,
                 container -> finish.apply(container.getExitMap())
+        );
+    }
+
+    /**
+     * This collector is not support input null elements
+     * and not support operations with parallel streams, only sequential operations with streams
+     * because we need do operations with elements that close to each other
+     *
+     * @param valListCreate the supplier that returns type of result collection
+     * @param <T> type of input elements
+     * @param <L> type of result collection
+     * @param <R> type of finished result
+     * @return a new Collector
+     */
+    public static <T, L extends Collection<T>, R> Collector<T, ?, R> doOperationWithSameObjs(
+            Supplier<L> valListCreate,
+            BinaryOperator<T> operation,
+            Function<? super L,? extends R> finish) {
+        class Container {
+            private final Map<Integer, L> samples;
+            private T element;
+            private int index;
+
+            Container() {
+                samples = new HashMap<>();
+            }
+
+            void add(T el) {
+                if (element == null) {
+                    L valList = valListCreate.get();
+                    valList.add(el);
+                    samples.put(index, valList);
+                    element = el;
+                } else {
+                    if (el.equals(element)) {
+                        samples.get(index).add(el);
+                    } else {
+                        L valList = valListCreate.get();
+                        valList.add(el);
+                        samples.put(++index, valList);
+                        element = el;
+                    }
+                }
+            }
+
+            Container combine(Container other) {
+                throw new UnsupportedByParallelCollector();
+            }
+
+            public L doOperation(){
+                return samples.values().stream().
+                        map(o -> o.stream().reduce(operation).orElse(null)).
+                        collect(Collectors.toCollection(valListCreate));
+            }
+        }
+        return Collector.of(
+                Container::new,
+                Container::add,
+                Container::combine,
+                o -> finish.apply(o.doOperation())
         );
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,22 @@
 import RToolsToCollect.RCollectors;
 
-import java.util.Comparator;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class main {
     public static void main(String[] args) {
-        String s = new Random().ints(5, 40).parallel().distinct().
-                limit(20).peek(System.out::println).boxed().collect(RCollectors.minMax(Integer::compare)).
-                orElse(null).converter((o1, o2) -> "Min : " + o1 + ", max : " + o2);
-        System.out.println(s);
+//        String s = new Random().ints(5, 40).parallel().distinct().
+//                limit(20).peek(System.out::println).boxed().collect(RCollectors.minMax(Integer::compare)).
+//                orElse(null).converter((o1, o2) -> "Min : " + o1 + ", max : " + o2);
+//        System.out.println(s);
+        List<Integer> l = Arrays.asList(1, 1, 1, 3, 2, 2, 2, 0, 1, 6, 6, 6);
+        String mapka = l.stream().collect(RCollectors.doOperationWithSameObjs(
+                ArrayList::new,
+                (o1, o2) -> o1 - o2,
+                o -> o.stream().map(String::valueOf).collect(Collectors.joining(", "))
+        ));
+        System.out.println(mapka);
     }
 }
